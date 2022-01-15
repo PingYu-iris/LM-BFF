9c43331ac935d06583e9b4c87a606310bae4d0b2
User <dxn0714@ubisec-dl.cse.buffalo.edu>
2022-01-15 01:20:08 -0500

    add

--- experiments.sh
+++ experiments.sh
@@ -3,21 +3,21 @@
 # lr in 1e-5 2e-5 5e-5
 # task in SST-2 sst-5 mr cr mpqa trec SNLI QNLI QQP
 
-for seed in 21
+for seed in 13 21 42 87 100
 do
     for bs in 2
     do
         for lr in 2e-5
         do
-            for k in 2
+            for k in 1 2
             do
-                for soft_prompt_tokens in 30
+                for soft_prompt_tokens in 25
                 do
-                    for model_type in prompt-tuning
+                    for model_type in prefix-tuning-stanford
                     do
                         for max_step in 500
                         do
-                            for task in SST-2
+                            for task in SST-2 sst-5 mr cr mpqa trec SNLI QNLI QQP
                             do
                                 TAG=exp \
                                 TYPE=$model_type \
